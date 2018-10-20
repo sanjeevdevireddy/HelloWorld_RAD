9fe23d0c3ed5a8b5689bc248bbf14e01bd839e1e
sanjeevdevireddy <devireddy.sanjeev@gmail.com>
2018-10-20 14:24:55 +0530

    First commit to the branch named firstBranch

--- HelloWorld_RAD/src/com/test/First.java
+++ HelloWorld_RAD/src/com/test/First.java
@@ -3,6 +3,7 @@ package com.test;
 public class First {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hi!! ");
+		System.out.println("Pointing to the branch named firstBranch");
 	}
 }
